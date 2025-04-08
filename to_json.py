dbbf4e2dedc3fe3b8e6899926f96f25974a00292
papplegate <papplega@gmail.com>
2025-04-07 20:41:51 -0400

    more complex line, better regexes

--- to_json.py
+++ to_json.py
@@ -1,11 +1,10 @@
 import re
-line = "(SORRY ((0) (PLEASE DON'T APOLIGIZE) (APOLOGIES ARE NOT NECESSARY) (WHAT FEELINGS DO YOU HAVE WHEN YOU APOLOGIZE) (I'VE TOLD YOU THAT APOLOGIES ARE NOT REQUIRED)))"
+line = "(REMEMBER 5 ((0 YOU REMEMBER 0) (DO YOU OFTEN THINK OF 4) (DOES THINKING OF 4 BRING ANYTHING ELSE TO MIND) (WHAT ELSE DO YOU REMEMBER) (WHY DO YOU REMEMBER 4 JUST NOW) (WHAT IN THE PRESENT SITUATION REMINDS YOU OF 4) (WHAT IS THE CONNECTION BETWEEN ME AND 4)) ((0 DO I REMEMBER 0) (DID YOU THINK I WOULD FORGET 5) (WHY DO YOU THINK I SHOULD RECALL 5 NOW) (WHAT ABOUT 5) (=WHAT) (YOU MENTIONED 5)) ((0) (NEWKEY)))"
 substitutions = {
-    r'([^\(^\)^ ]+)': r'"\1",',
-    r'(\)+)': r'\1,',
+    r'\(([\w\d ]+?) \(': r'("\1", (',
+    r'\(([^\(]+?)\)': r'"\1",',  # replace parenthesized words/groups with strings
+    r'\) \(': r'), (',  # add commas between "tuples"
 }
 for pattern, replacement in substitutions.items():
     line = re.compile(pattern).sub(replacement, line)
-exec('line = ' + line)
-line = {line[0][0]: {line[0][1][0][0]: [' '.join(element) for element in line[0][1]]}} 
 print(line)
