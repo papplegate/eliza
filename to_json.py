e2bca67b9f666f196e933817ec3c98f4d2973772
papplegate <papplega@gmail.com>
2025-04-11 18:51:31 -0400

    loop over lines

--- to_json.py
+++ to_json.py
@@ -1,10 +1,12 @@
 import re
-line = "(REMEMBER 5 ((0 YOU REMEMBER 0) (DO YOU OFTEN THINK OF 4) (DOES THINKING OF 4 BRING ANYTHING ELSE TO MIND) (WHAT ELSE DO YOU REMEMBER) (WHY DO YOU REMEMBER 4 JUST NOW) (WHAT IN THE PRESENT SITUATION REMINDS YOU OF 4) (WHAT IS THE CONNECTION BETWEEN ME AND 4)) ((0 DO I REMEMBER 0) (DID YOU THINK I WOULD FORGET 5) (WHY DO YOU THINK I SHOULD RECALL 5 NOW) (WHAT ABOUT 5) (=WHAT) (YOU MENTIONED 5)) ((0) (NEWKEY)))"
 substitutions = {
-    r'\(([\w\d ]+?) \(': r'("\1", (',
     r'\(([^\(]+?)\)': r'"\1",',  # replace parenthesized words/groups with strings
+    r'([\w\d]++)(?<!\")(?!\")': r'"\1",', # get any words/numbers we missed
     r'\) \(': r'), (',  # add commas between "tuples"
 }
-for pattern, replacement in substitutions.items():
-    line = re.compile(pattern).sub(replacement, line)
-print(line)
+
+with open('weizenbaum_1966_appendix.txt', 'r') as appendix:
+    for line in appendix:
+        for pattern, replacement in substitutions.items():
+            line = re.compile(pattern).sub(replacement, line)
+        print(line)
