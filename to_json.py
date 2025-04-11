ed47f2bab09e0e22df2c673122b9950d930bde61
papplegate <papplega@gmail.com>
2025-04-11 19:16:45 -0400

    correctly print each line

--- to_json.py
+++ to_json.py
@@ -1,12 +1,11 @@
 import re
 substitutions = {
-    r'\(([^\(]+?)\)': r'"\1",',  # replace parenthesized words/groups with strings
-    r'([\w\d]++)(?<!\")(?!\")': r'"\1",', # get any words/numbers we missed
-    r'\) \(': r'), (',  # add commas between "tuples"
+    r'([^() \n]+)': r'"\1",',  # delineate "strings" 
+    r'\)': r'),',  # add commas after "tuples"
 }
 
 with open('weizenbaum_1966_appendix.txt', 'r') as appendix:
-    for line in appendix:
+    for line in appendix.readlines()[2:]:
         for pattern, replacement in substitutions.items():
             line = re.compile(pattern).sub(replacement, line)
-        print(line)
+        print(line, end='')
