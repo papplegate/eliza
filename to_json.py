5602f04cf8acc364ba69c50af77facdb9542fe22
papplegate <papplega@gmail.com>
2025-04-13 21:27:20 -0400

    all lines parse successfully

--- to_json.py
+++ to_json.py
@@ -1,11 +1,14 @@
 import re
 substitutions = {
     r'([^() \n]+)': r'"\1",',  # delineate "strings" 
-    r'\)': r'),',  # add commas after "tuples"
+    r'\)(?!\n)': r'),',  # add commas after "tuples"
 }
 
 with open('weizenbaum_1966_appendix.txt', 'r') as appendix:
-    for line in appendix.readlines()[2:]:
+    for i, line in enumerate(appendix):
+        if i < 2:
+            continue
         for pattern, replacement in substitutions.items():
             line = re.compile(pattern).sub(replacement, line)
-        print(line, end='')
+        exec('line = ' + line)
+        print(line)
