5bf2c9cf1d2f14c6fcf74554fcc64d494c0b9064
papplegate <papplega@gmail.com>
2025-04-17 22:56:05 -0400

    Use a stack to better handle adjacent, related entries

--- to_json.py
+++ to_json.py
@@ -16,10 +16,25 @@ with open('weizenbaum_1966_appendix.txt', 'r') as appendix:
             line = re.compile(pattern).sub(replacement, line)
         exec('parsed = ' + line)
 
-        result[parsed[0]] = {}
+        keyword = parsed[0]
+        result[keyword] = {}
 
-        for element in parsed[1:]:
-            if type(element) == str and re.compile(r'\d+').search(element):
-                result[parsed[0]]['score'] = int(element)
+        stack = list(parsed[1:])
+        stack.reverse()
+        while len(stack):
+            element = stack.pop()
+            if type(element) == str:
+                if re.compile(r'\d+').search(element):
+                    result[keyword]['score'] = int(element)
+                elif element == '=':
+                    result[keyword]['replace'] = stack.pop()
+                else:
+                    print(f"No match for {element} in {parsed}")
+        # types = []
+        # for element in parsed[1:]:
+        #     types.append(str(type(element)))
+        #     if type(element) == str and re.compile(r'\d+').search(element):
+        #         result[parsed[0]]['score'] = int(element)
+        # print(types)
 
 print(result)
